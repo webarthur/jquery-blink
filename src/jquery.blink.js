3375e784cc0d6f5b588e80b0ae473417eae717fa
Arthur Araújo <webarthur@gmail.com>
2015-06-04 22:45:07 -0300

    v1.0

--- src/jquery.blink.js
+++ src/jquery.blink.js
@@ -8,16 +8,16 @@ jQuery.fn.extend({
 	blink: function(params1, params2, timer) {
 		return this.each(function() {
 			var e = jQuery(this);
-			if(params1)e.data('_blick_params1', params1);
-			if(params2)e.data('_blick_params2', params2);
-			if(timer)e.data('_timer', timer);
+			if(params1)e.data('_blink_params1', params1);
+			if(params2)e.data('_blink_params2', params2);
+			if(timer)e.data('_blink_timer', timer);
 			e.css('transition', 'none');
-			if( e.data('_blick_state') ) {
-				e.animate(e.data('_blick_params2'), e.data('_blick_timer'), function(){ e.blink(); } );
-				e.data('_blick_state', 0);
+			if( e.data('_blink_state') ) {
+				e.animate(e.data('_blink_params2'), e.data('_blink_timer'), function(){ e.blink(); } );
+				e.data('_blink_state', 0);
 			} else {
-				e.animate(e.data('_blick_params1'), e.data('_blick_timer'), function(){ e.blink(); } );
-				e.data('_blick_state', 1);
+				e.animate(e.data('_blink_params1'), e.data('_blink_timer'), function(){ e.blink(); } );
+				e.data('_blink_state', 1);
 			}
 		});
 	}
