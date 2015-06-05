bd62a0028b990675d70037aaa07d5e50b653c7af
Arthur Araújo <webarthur@gmail.com>
2015-06-04 22:22:54 -0300

    v1.0

---
+++ src/jquery.blink.js
@@ -0,0 +1,18 @@
+jQuery.fn.extend({
+	blink: function(params1, params2, timer) {
+		return this.each(function() {
+			var e = jQuery(this);
+			if(params1)e.data('_blick_params1', params1);
+			if(params2)e.data('_blick_params2', params2);
+			if(timer)e.data('_timer', timer);
+			e.css('transition', 'none');
+			if( e.data('_blick_state') ) {
+				e.animate(e.data('_blick_params2'), e.data('_blick_timer'), function(){ e.blink(); } );
+				e.data('_blick_state', 0);
+			} else {
+				e.animate(e.data('_blick_params1'), e.data('_blick_timer'), function(){ e.blink(); } );
+				e.data('_blick_state', 1);
+			}
+		});
+	}
+});
